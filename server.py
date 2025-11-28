e92d16c6d9e9a84d5a2381228e627531999c7f37
hackr <hackr8027@gmail.com>
2025-11-28 17:56:24 -0500

    fixed a little somthing

--- server.py
+++ server.py
@@ -3,25 +3,26 @@ import flask
 
 app = flask.Flask(__name__, static_folder='public')
 
-@app.route('/<path:filename>', methods=['GET'])
-def serve_static(filename):
-    return flask.send_from_directory(app.static_folder, filename)
-
 @app.route('/', methods=['GET'])
 def home():
     return flask.send_file('index.html')
-
 
 @app.route('/main.css', methods=['GET'])
 def styling():
     return flask.send_file('main.css', mimetype='text/css')
 
-@app.route('/public/fonts', methods=['GET'])
-def fonts():
-    return flask.send_file('public/fonts', mimetype='font/ttf')
-
 @app.route('/events', methods=['GET'])
 def events():
     return flask.send_file('events.html')
 
-app.run(host='localhost', port=3000)
+@app.route('/events/anonymous', methods=['GET'])
+def anonymous_event():
+    return flask.send_file('anonymousanomaly.html')
+
+@app.route('/members', methods=['GET'])
+def members():
+    return flask.send_file('members/index.html')
+
+# Run the app
+if __name__ == '__main__':
+    app.run(host='localhost', port=3000, debug=True)
