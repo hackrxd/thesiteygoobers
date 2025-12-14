db8f1910d3815432a0c1e6a0228f5db30379a718
hackr <hackr8027@gmail.com>
2025-12-14 03:39:28 -0500

    allowed /deploy to be called by myself

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ app = Flask(__name__, static_folder=PUBLIC_DIR)
 app.config['UPLOAD_FOLDER'] = UPLOAD_DIR
 maintenence = False
 
-@app.route('/deploy', methods=['POST'])
+@app.route('/deploy')
 def webhook_deploy():
     # Optional: validate GitHub secret
     subprocess.call(['/home/hackr/deploy.sh'])
